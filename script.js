3334f9975f24e8a347ea3f737e848ebafcda07dd
uyennnnnn <buiuyen1207@gmail.com>
2022-04-25 09:05:34 -0700

    uyen

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Uyen");
+
+alert("Uyen")
+
+var name=prompt("Nhap ten");
+var age=prompt("Nhap tuoi");
+var address=prompt("Nhap dia chi");
+console.log(name+age+address)
+
+// var u =1;
+// u=u+1;
+// console.log(u);
+// var name ="uyen";
+// var age = 20;
+// var address = 206;
+
+// console.log(name+age+address)
